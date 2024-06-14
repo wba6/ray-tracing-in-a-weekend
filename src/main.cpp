9e752ea38c7a28f55aab7ee29fc026f19cd4e19c
William.A <bradenaey1@gmail.com>
2024-06-13 21:06:50 -0400

    chapter 3 basic class setups

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "color.h"
+#include "math/vec3.h"
 
 int main() {
 
@@ -12,16 +14,12 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScalines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1),0);
+            write_color(std::cout, pixel_color);
         }
     }
+
+    std::clog << "\rDone.                   \n";
 }
