c445b59594ceb178f08817b9bf988570b5b3971e
William.A <bradenaey1@gmail.com>
2024-07-02 10:07:51 -0400

    new camera class

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "rtweekend.h"
 #include "sphere.h"
 #include "square.h"
+#include "camera.h"
 
 color ray_color(const ray &r, const hittable &world)
 {
@@ -19,16 +20,6 @@ color ray_color(const ray &r, const hittable &world)
 
 int main()
 {
-
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
-
     // World - sample list of hittable objects
     hittable_list world;
 
@@ -36,42 +27,10 @@ int main()
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
     //world.add(make_shared<square>(point3(0, -25, -5), 50));
 
-    // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center = point3(0, 0, 0);
+    camera cam;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
 
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-    std::cout << "P3\n"
-              << image_width << " " << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++)
-    {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++)
-        {
-            //move from top right to bottom left shadding of pixels
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::clog << "\rDone.                 \n";
+    cam.render(world);
 }
